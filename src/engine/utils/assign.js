db30abb34d72348001e856107f70a1506f1f6d17
Aaron Hardy <aahardy@adobe.com>
2015-05-13 17:08:32 -0600

    Updated assign to accept any number of source objects to be more aligned with the ES6 proposal.

--- src/engine/utils/assign.js
+++ src/engine/utils/assign.js
@@ -1,6 +1,14 @@
-module.exports = function(dst, src){
-  for (var prop in src)
-    if (src.hasOwnProperty(prop))
-      dst[prop] = src[prop]
-  return dst;
+module.exports = function(target){
+  for (var i = 1; i < arguments.length; i++) {
+    var from = arguments[i];
+    if (from === null || from === undefined) {
+      continue;
+    }
+    for (var key in from) {
+      if (from.hasOwnProperty(key)) {
+        target[key] = from[key];
+      }
+    }
+  }
+  return target;
 };

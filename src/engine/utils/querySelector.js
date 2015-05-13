2416248d3f5b2a957cd78845a45845ce7cfb1af1
Aaron Hardy <aahardy@adobe.com>
2015-05-13 16:42:15 -0600

    Removing ensureQuerySelector as it probably won't be necessary.

--- src/engine/utils/querySelector.js
+++ src/engine/utils/querySelector.js
@@ -1,10 +1,2 @@
 // TODO: Add Sizzle support. Does this need to be async?
-//var querySelectorPromise = require('../private/ensureQuerySelector')();
-//
-//module.exports = function (selector, callback){
-//  querySelectorPromise.then(function() {
-//    callback(document.querySelector(selector));
-//  });
-//};
-
 module.exports = document.querySelector.bind(document);
--- src/engine/utils/querySelectorAll.js
+++ src/engine/utils/querySelectorAll.js
@@ -1,10 +1,2 @@
 // TODO: Add Sizzle support. Does this need to be async?
-//var querySelectorPromise = require('../private/ensureQuerySelector')();
-//
-//module.exports = function (selector, callback){
-//  querySelectorPromise.then(function() {
-//    callback(document.querySelectorAll(selector));
-//  });
-//};
-
 module.exports = document.querySelectorAll.bind(document);

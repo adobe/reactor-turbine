5c1dad70ddcc843841ff5bb0f3e805506de58c62
Aaron Hardy <aahardy@adobe.com>
2015-05-20 13:11:39 -0600

    Added "add like button" support to facebook connect extension.

--- src/config/facebookConnect/core.js
+++ src/config/facebookConnect/core.js
@@ -41,6 +41,31 @@ function create(extensionSettings) {
           FB.XFBML.parse();
         }
       });
+    },
+    addLikeButton: function(actionSettings) {
+      promise.then(function() {
+        var container = document.querySelector(actionSettings.selector);
+
+        if (container) {
+          var div = document.createElement('div');
+          div.classList.add('fb-like');
+          div.setAttribute('data-action', 'like');
+          div.setAttribute('data-href', actionSettings.href);
+          div.setAttribute('data-layout', actionSettings.layout);
+          div.setAttribute('data-show-faces', actionSettings.showFaces);
+          div.setAttribute('data-share', actionSettings.share);
+
+          if (actionSettings.hasOwnProperty('width')) {
+            div.setAttribute('data-width', actionSettings.width);
+          }
+
+          container.appendChild(div);
+
+          engine.parseXFBML({
+            selector: actionSettings.selector
+          });
+        }
+      });
     }
   };
 

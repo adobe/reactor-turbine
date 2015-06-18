73b6a4bf5710bfed51d6ac21236093618ef7b073
Aaron Hardy <aahardy@adobe.com>
2015-06-18 16:12:26 -0600

    Added more docs.

--- src/config/dtm/conditions/custom.js
+++ src/config/dtm/conditions/custom.js
@@ -1,3 +1,12 @@
+/**
+ * Custom condition. This executes a condition script provided by the user.
+ * @param {Object} settings Condition settings.
+ * @param {Function} settings.script The custom script function.
+ * @param {Object} [event] The underlying event object the triggered the rule.
+ * @param {Object} [event.target] The element where the event originated.
+ * @param {HTMLElement} [relatedElement] The element the rule was targeting.
+ * @returns {boolean}
+ */
 module.exports = function(settings, event, relatedElement) {
   return settings.script.call(relatedElement, event, event ? event.target : undefined);
 };
--- src/config/dtm/conditions/urlParameter.js
+++ src/config/dtm/conditions/urlParameter.js
@@ -1,6 +1,14 @@
 var textMatch = require('textMatch');
 var getQueryParam = require('getQueryParam');
 
+/**
+ * URL parameter condition. Determines if there is a querystring parameter with a matching name
+ * and value.
+ * @param {Object} settings Condition settings.
+ * @param {string} settings.name The name of the querystring parameter.
+ * @param {string} settings.value The value of the querystring parameter.
+ * @returns {boolean}
+ */
 module.exports = function(settings) {
   return textMatch(getQueryParam(settings.name), settings.value);
 };

b17fdeb43cff4613944afde46bee86525071ef22
Aaron Hardy <aahardy@adobe.com>
2015-06-22 13:16:26 -0600

    Reformatting.

--- src/engine/utils/dom/matchesCSS.js
+++ src/engine/utils/dom/matchesCSS.js
@@ -1,9 +1,11 @@
 module.exports = (function(docEl) {
 
   function simpleTagMatch(selector, elm) {
-    var tagName = elm.tagName
-    if (!tagName) return false
-    return selector.toLowerCase() === tagName.toLowerCase()
+    var tagName = elm.tagName;
+    if (!tagName) {
+      return false;
+    }
+    return selector.toLowerCase() === tagName.toLowerCase();
   }
 
   var matches =
@@ -11,36 +13,42 @@ module.exports = (function(docEl) {
     docEl.mozMatchesSelector ||
     docEl.webkitMatchesSelector ||
     docEl.oMatchesSelector ||
-    docEl.msMatchesSelector
+    docEl.msMatchesSelector;
   if (matches) {
     return function(selector, elm) {
-      if (elm === document || elm === window) return false
-      try {
-        return matches.call(elm, selector)
-      } catch (e) {
-        return false
+      if (elm === document || elm === window) {
+        return false;
       }
-    }
+      try {
+        return matches.call(elm, selector);
+      } catch (e) {
+        return false;
+      }
+    };
   } else if (docEl.querySelectorAll) {
     return function(selector, elm) {
-      var parent = elm.parentNode
-      if (!parent) return false
+      var parent = elm.parentNode;
+      if (!parent) {
+        return false;
+      }
       if (selector.match(/^[a-z]+$/i)) {
-        return simpleTagMatch(selector, elm)
+        return simpleTagMatch(selector, elm);
       }
       try {
-        var nodeList = elm.parentNode.querySelectorAll(selector)
+        var nodeList = elm.parentNode.querySelectorAll(selector);
         for (var i = nodeList.length; i--;)
-          if (nodeList[i] === elm) return true
+          if (nodeList[i] === elm) {
+            return true;
+          }
       } catch (e) {
         //
       }
-      return false
-    }
+      return false;
+    };
   } else {
     return function(selector, elm) {
       if (selector.match(/^[a-z]+$/i)) {
-        return simpleTagMatch(selector, elm)
+        return simpleTagMatch(selector, elm);
       }
       // TODO: Re-implement once we start loading sizzle.
       //try {
@@ -48,6 +56,6 @@ module.exports = (function(docEl) {
       //} catch (e) {
       //  return false
       //}
-    }
+    };
   }
-}(document.documentElement))
+}(document.documentElement));

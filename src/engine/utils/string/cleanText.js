f6721734c6d10edc7482c4daf235d0e9341b677a
Aaron Hardy <aahardy@adobe.com>
2015-06-22 15:33:10 -0600

    Updating cleanText to be aligned with previous engine.

--- src/engine/utils/string/cleanText.js
+++ src/engine/utils/string/cleanText.js
@@ -1,18 +1,11 @@
-var trim = require('./trim');
-
 // `cleanText(str)`
 // ================
 //
-// "Cleans" the text from an element's innerText by removing non-ASCII characters, replacing
-// double or more spaces with a single space, and removing leading and trailing spaces. This is
-// used directly by the @cleanText special property.
+// "Cleans" text by trimming the string and removing spaces and newlines.
 module.exports = function(str) {
   if (str == null) {
     return null;
   }
-  return trim(str
-    // Removes non-ASCII characters
-    .replace(/[^\000-\177]*/g, '')
-    // Replaces any double or more spaces with a single space
-    .replace(/\s{2,}/g, ' '));
+
+  return str.replace(/\s+/g, ' ').trim();
 };

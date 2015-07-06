df013f6e5071812f24f707d40df625550ca6f1e4
jaarchib <jaarchib@adobe.com>
2015-07-06 16:03:31 -0600

    object tests.  for assigns and keys

--- src/engine/utils/object/__tests__/assign.test.js
+++ src/engine/utils/object/__tests__/assign.test.js
@@ -1,30 +1,31 @@
 var assign = require('../assign');
 
-var target = {a:'apple', b:'banana', c:'cucumber'};
-var target2 = {d:'dandilion', a:'apricot'};
+var target = {a: 'apple', b: 'banana', c: 'cucumber'};
 
 describe('assigns', function() {
   it('returns new property list for own properties', function() {
 
-    function Fruit() {
-      this.color = 'red';
-    };
+    function Apple() {
+      this.color = 'green';
+    }
 
-    var fruity = new Fruit();
-    var moreFruity = new Fruit();
+    Apple.prototype = {a: 'honeycrisp'};
 
-    Fruit.prototype = target;
+    var apple = new Apple();
+    var res = assign(target, apple);
 
-    var res = assign(fruity, target);
-
-    expect(res).toEqual(fruity);
-    expect(moreFruity).not.toEqual(fruity);
+    expect(res.color).toEqual('green');
+    expect(res.a).toEqual('apple');
   });
 
-  it('returns a new property list replacing key with new value', function() {
-    var res = assign(target, target2, {e:'eclair'});
+  it('returns a new property list with all values', function() {
+    var res = assign(target, {e: 'eclair'});
+    expect(res).toBe(target);
+  });
 
-    expect(res).toEqual(target);
+  it('value of last property overrides previous.', function() {
+    var res = assign(target, {a: 'apricot'});
+    expect(res.a).toEqual('apricot');
   });
 
 });

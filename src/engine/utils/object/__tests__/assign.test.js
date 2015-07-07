6b880dc3339bb4dd85b37faf8e4ad1b7c1143d91
jaarchib <jaarchib@adobe.com>
2015-07-07 08:03:31 -0600

    modified a few tests based on aarons comments.

--- src/engine/utils/object/__tests__/assign.test.js
+++ src/engine/utils/object/__tests__/assign.test.js
@@ -18,14 +18,24 @@ describe('assigns', function() {
     expect(res.a).toEqual('apple');
   });
 
-  it('returns a new property list with all values', function() {
+  it('returns an object reference to the target', function() {
     var res = assign(target, {e: 'eclair'});
     expect(res).toBe(target);
   });
 
+  it('returns all values from all lists', function() {
+    var other = {someFruit: 'nachos'};
+    var res = assign(target, {e: 'elderberry'}, other);
+    expect(res.a).toEqual('apple');
+    expect(res.b).toEqual('banana');
+    expect(res.c).toEqual('cucumber');
+    expect(res.e).toEqual('elderberry');
+    expect(res.someFruit).toEqual('nachos');
+  });
+
   it('value of last property overrides previous.', function() {
-    var res = assign(target, {a: 'apricot'});
-    expect(res.a).toEqual('apricot');
+    var res = assign(target, {a: 'apricot'}, {d: 'date'}, {a: 'avocado'});
+    expect(res.a).toEqual('avocado');
   });
 
 });

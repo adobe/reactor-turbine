0da3ab8b70b284a4df4970376227904ca4fe4a9b
jaarchib <jaarchib@adobe.com>
2015-07-14 15:50:36 -0600

    made classList tests better

--- src/engine/utils/dom/__tests__/classList.test.js
+++ src/engine/utils/dom/__tests__/classList.test.js
@@ -1,123 +1,97 @@
 var classList = require('../classList');
-var testElement;
 
 describe('classList', function() {
-  // add tests
-  it('add: returns a className when the className is not defined', function() {
+  var testElement;
+
+  beforeAll(function() {
     testElement = document.createElement('p');
     testElement.id = 'test';
-    document.body.appendChild(testElement);
-
-    classList.add(testElement, 'fooMan');
-    expect(testElement.className).toEqual(' fooMan');
   });
 
-  it('add: appends the className if already defined to the returned string', function() {
-    testElement = document.createElement('p');
-    testElement.id = 'test';
-    testElement.className = 'fooMan';
-    document.body.appendChild(testElement);
-
-    classList.add(testElement, 'fooBoy');
-    expect(testElement.className).toEqual('fooMan fooBoy');
+  beforeEach(function() {
+    testElement.className = '';
   });
 
-  it('add: returns a className with a space when the className is not defined', function() {
-    testElement = document.createElement('p');
-    testElement.id = 'test';
-    testElement.className = 'foo Man';
-    document.body.appendChild(testElement);
+  describe('add()', function() {
+    // add tests
+    it('returns a className when the className is not previously defined', function() {
+      testElement.className = 'fooMan';
+      classList.add(testElement, 'fooMan');
+      expect(testElement.className).toEqual('fooMan');
+    });
 
-    classList.add(testElement, 'foo Man');
-    expect(testElement.className).toEqual('foo Man');
-  });
+    it('appends the className if already defined, to the returned string', function() {
+      testElement.className = 'fooMan';
+      classList.add(testElement, 'fooBoy');
+      expect(testElement.className).toEqual('fooMan fooBoy');
+    });
 
-  it('add: returns TypeError if the target element is not a string.', function() {
-    var throwsError = function() {
-      classList.add({a: 'apple'}, 'some className');
-    };
-    expect(throwsError).toThrowError(TypeError);
+    it('returns a className with a space when the className is not previously defined', function() {
+      testElement.className = 'foo Man';
+      classList.add(testElement, 'foo Man');
+      expect(testElement.className).toEqual('foo Man');
+    });
+
+    it('returns TypeError if the target element is not a string.', function() {
+      var throwsError = function() {
+        classList.add({a: 'apple'}, 'some className');
+      };
+      expect(throwsError).toThrowError(TypeError);
+    });
   });
 
   //contains tests
-  it('contains: returns null if the element does not have the specified className', function() {
-    testElement = document.createElement('p');
-    testElement.id = 'test';
-    testElement.className = 'fooMan';
-    document.body.appendChild(testElement);
+  describe('contains()', function() {
+    it('returns null if the element does not have the specified className', function() {
+      testElement.className = 'fooMan';
+      expect(classList.contains(testElement, 'fooBoy')).toBeNull();
+    });
 
-    expect(classList.contains(testElement, 'fooBoy')).toBeNull();
-  });
+    it('returns null if the className is NOT set.', function() {
+      expect(classList.contains(testElement, 'className')).toBeNull();
+    });
 
-  it('contains: returns null if the className is NOT set.', function() {
-    testElement = document.createElement('div');
-    testElement.id = 'test';
-
-    expect(classList.contains(testElement, 'className')).toBeNull();
-  });
-
-  it('contains: returns null if the className is not a string.', function() {
-    testElement = document.createElement('div');
-    testElement.id = 'test';
-    testElement.className = {a: 'apple'};
-
-    expect(classList.contains(testElement, {a: 'apple'})).toBeNull();
+    it('returns null if the className is not a string.', function() {
+      testElement.className = {a: 'apple'};
+      expect(classList.contains(testElement, {a: 'apple'})).toBeNull();
+    });
   });
 
   //toggle tests
-  it('toggle: adds the className if it does not exist.', function() {
-    testElement = document.createElement('p');
-    testElement.id = 'test';
-    document.body.appendChild(testElement);
+  describe('toggle()', function() {
+    it('adds the className if it does not exist.', function() {
+      classList.toggle(testElement, 'fooMan');
+      expect(testElement.className).toEqual(' fooMan');
+    });
 
-    classList.toggle(testElement, 'fooMan');
-    expect(testElement.className).toEqual(' fooMan');
+    it('removes the className if it already exists.', function() {
+      testElement.className = 'fooMan';
+      classList.toggle(testElement, 'fooMan');
+      expect(testElement.className).toEqual('');
+    });
+
+    it('removes the className if multiples already exists.', function() {
+      testElement.className = 'foo Man';
+      classList.toggle(testElement, 'Man');
+      expect(testElement.className).toEqual('foo ');
+
+      classList.add(testElement, 'Man');
+      expect(testElement.className).toEqual('foo  Man'); // double spaces
+
+    });
   });
 
-  it('toggle: removes the className if it already exists.', function() {
-    testElement = document.createElement('p');
-    testElement.id = 'test';
-    testElement.className = 'fooMan';
-    document.body.appendChild(testElement);
+  describe('delete()', function() {
+    it('removes the className if it already exists.', function() {
+      testElement.className = 'fooMan';
+      classList.remove(testElement, 'fooMan');
+      expect(testElement.className).toEqual('');
+    });
 
-    classList.toggle(testElement, 'fooMan');
-    expect(testElement.className).toEqual('');
+    it('removes only one if multiple classNames exists.', function() {
+      testElement.className = 'foo Man';
+      classList.remove(testElement, 'foo');
+      expect(testElement.className).toEqual(' Man');
+    });
   });
-
-  it('toggle: removes the className if multiples already exists.', function() {
-    testElement = document.createElement('p');
-    testElement.id = 'test';
-    testElement.className = 'foo Man';
-    document.body.appendChild(testElement);
-
-    classList.toggle(testElement, 'Man');
-    expect(testElement.className).toEqual('foo ');
-
-    classList.add(testElement, 'Man');
-    expect(testElement.className).toEqual('foo  Man'); // double spaces
-
-  });
-
-  // remove tests
-  it('remove: removes the className if it already exists.', function() {
-    testElement = document.createElement('p');
-    testElement.id = 'test';
-    testElement.className = 'fooMan';
-    document.body.appendChild(testElement);
-
-    classList.remove(testElement, 'fooMan');
-    expect(testElement.className).toEqual('');
-  });
-
-  it('remove: removes only one if multiple classNamesexists.', function() {
-    testElement = document.createElement('p');
-    testElement.id = 'test';
-    testElement.className = 'foo Man';
-    document.body.appendChild(testElement);
-
-    classList.remove(testElement, 'foo');
-    expect(testElement.className).toEqual(' Man');
-  });
-
-
 });

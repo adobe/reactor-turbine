a8df8aea759659df97a1cefa38712b5647237c58
Aaron Hardy <aahardy@adobe.com>
2015-08-17 11:22:20 -0600

    Improved subdomain condition documentation.

--- src/extensions/dtm/conditions/subdomain/subdomain.js
+++ src/extensions/dtm/conditions/subdomain/subdomain.js
@@ -7,10 +7,10 @@ var document = require('document');
  * Subdomain condition. Determines if the current location is an matching subdomain.
  * @param {Object} config
  * @param {Object} config.conditionConfig Condition config.
- * @param {(RegEx|string)[]} config.conditionConfig.include An array of possible subdomains that the current
- * subdomain must match.
- * @param {(RegEx|string)[]} config.conditionConfig.exclude An array of possible subdomains that the current
- * subdomain must not match.
+ * @param {(RegEx|string)[]} [config.conditionConfig.include] An array of subdomains. If specified,
+ * the current domain must match at least one of the listed subdomains.
+ * @param {(RegEx|string)[]} [config.conditionConfig.exclude] An array of subdomains. If specified,
+ * the current domain must not match any of the listed subdomains.
  * @returns {boolean}
  */
 module.exports = function(config) {

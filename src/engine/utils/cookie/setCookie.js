c50b7ee5f9f6c677fdaca1366e6db4e12f64164d
Aaron Hardy <aahardy@adobe.com>
2015-08-18 10:21:22 -0600

    Improved setCookie docs.

--- src/engine/utils/cookie/setCookie.js
+++ src/engine/utils/cookie/setCookie.js
@@ -2,7 +2,8 @@
  * Writes a cookie.
  * @param {string} name The name of the cookie to save.
  * @param {string} value The value of the cookie to save.
- * @param {number} days The number of days to store the cookie.
+ * @param {number} [days] The number of days to store the cookie. If not specified, the cookie will
+ * be stored for the session only.
  */
 module.exports = function(name, value, days) {
   var expires;

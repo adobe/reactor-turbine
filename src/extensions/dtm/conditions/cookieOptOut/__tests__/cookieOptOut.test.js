a2ac143534f2bf2742e1320fdde3fd03d6e7fcf6
Aaron Hardy <aahardy@adobe.com>
2015-08-18 13:13:54 -0600

    Added support for custom euCookieName in cookie opt-out condition.

--- src/extensions/dtm/conditions/cookieOptOut/__tests__/cookieOptOut.test.js
+++ src/extensions/dtm/conditions/cookieOptOut/__tests__/cookieOptOut.test.js
@@ -10,75 +10,95 @@ var setCookie = require('../../../../../engine/utils/cookie/setCookie');
 var removeCookie = require('../../../../../engine/utils/cookie/removeCookie');
 
 describe('cookie opt-out condition delegate', function() {
-  afterEach(function() {
-    removeCookie('sat_track');
-  });
+  var runTests = function(customCookieName) {
+    var propertyConfig = {};
+    var cookieName = 'sat_track';
 
-  it('returns true when the sat_cookie is set to "true" and acceptsCookies is true', function() {
-    setCookie('sat_track', 'true');
+    if (customCookieName) {
+      propertyConfig.euCookieName = customCookieName;
+      cookieName = customCookieName;
+    }
 
-    var config = {
-      conditionConfig: {
-        acceptsCookies: true
-      }
-    };
+    it('returns true when the sat_cookie is set to "true" and acceptsCookies is true', function() {
+      setCookie(cookieName, 'true');
 
-    expect(conditionDelegate(config)).toBe(true);
-  });
+      var config = {
+        conditionConfig: {
+          acceptsCookies: true
+        },
+        propertyConfig: propertyConfig
+      };
 
-  it('returns false when the sat_cookie is set to "false" and acceptsCookies is true', function() {
-    setCookie('sat_track', 'false');
+      expect(conditionDelegate(config)).toBe(true);
+      removeCookie(cookieName);
+    });
 
-    var config = {
-      conditionConfig: {
-        acceptsCookies: true
-      }
-    };
+    it('returns false when the sat_cookie is set to "false" and acceptsCookies is true', function() {
+      setCookie(cookieName, 'false');
 
-    expect(conditionDelegate(config)).toBe(false);
-  });
+      var config = {
+        conditionConfig: {
+          acceptsCookies: true
+        },
+        propertyConfig: propertyConfig
+      };
 
-  it('returns false when the sat_cookie is set to "true" and acceptsCookies is false', function() {
-    setCookie('sat_track', 'true');
+      expect(conditionDelegate(config)).toBe(false);
+      removeCookie(cookieName);
+    });
 
-    var config = {
-      conditionConfig: {
-        acceptsCookies: false
-      }
-    };
+    it('returns false when the sat_cookie is set to "true" and acceptsCookies is false', function() {
+      setCookie(cookieName, 'true');
 
-    expect(conditionDelegate(config)).toBe(false);
-  });
+      var config = {
+        conditionConfig: {
+          acceptsCookies: false
+        },
+        propertyConfig: propertyConfig
+      };
 
-  it('returns true when the sat_cookie is set to "false" and acceptsCookies is false', function() {
-    setCookie('sat_track', 'false');
+      expect(conditionDelegate(config)).toBe(false);
+      removeCookie(cookieName);
+    });
 
-    var config = {
-      conditionConfig: {
-        acceptsCookies: false
-      }
-    };
+    it('returns true when the sat_cookie is set to "false" and acceptsCookies is false', function() {
+      setCookie(cookieName, 'false');
 
-    expect(conditionDelegate(config)).toBe(true);
-  });
+      var config = {
+        conditionConfig: {
+          acceptsCookies: false
+        },
+        propertyConfig: propertyConfig
+      };
 
-  it('returns false when the sat_cookie has not been set and acceptsCookies is true', function() {
-    var config = {
-      conditionConfig: {
-        acceptsCookies: true
-      }
-    };
+      expect(conditionDelegate(config)).toBe(true);
+      removeCookie(cookieName);
+    });
 
-    expect(conditionDelegate(config)).toBe(false);
-  });
+    it('returns false when the sat_cookie has not been set and acceptsCookies is true', function() {
+      var config = {
+        conditionConfig: {
+          acceptsCookies: true
+        },
+        propertyConfig: propertyConfig
+      };
 
-  it('returns false when the sat_cookie has not been set and acceptsCookies is false', function() {
-    var config = {
-      conditionConfig: {
-        acceptsCookies: false
-      }
-    };
+      expect(conditionDelegate(config)).toBe(false);
+    });
 
-    expect(conditionDelegate(config)).toBe(false);
-  });
+    it('returns false when the sat_cookie has not been set and acceptsCookies is false', function() {
+      var config = {
+        conditionConfig: {
+          acceptsCookies: false
+        },
+        propertyConfig: propertyConfig
+      };
+
+      expect(conditionDelegate(config)).toBe(false);
+    });
+  };
+
+
+  runTests();
+  runTests('sat_custom_track');
 });

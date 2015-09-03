b5d78a75f7a6ddf37836d29de15c946749aeef8e
Aaron Hardy <aahardy@adobe.com>
2015-09-03 13:07:12 -0600

    Breaking out creation of config object into shared function.

--- src/extensions/dtm/conditions/pageViews/__tests__/pageViews.test.js
+++ src/extensions/dtm/conditions/pageViews/__tests__/pageViews.test.js
@@ -18,6 +18,16 @@ var DURATIONS = [
   'session'
 ];
 
+function getConfig(count, operator, duration) {
+  return {
+    conditionConfig: {
+      count: count,
+      operator: operator,
+      duration: duration
+    }
+  };
+}
+
 describe('page views condition delegate', function() {
   beforeEach(function() {
     mockVisitorTracking.getLifetimePageViewCount.calls.reset();
@@ -35,79 +45,37 @@ describe('page views condition delegate', function() {
       };
 
       it('returns true when number of page views is above "greater than" constraint', function() {
-        var config = {
-          conditionConfig: {
-            count: 4,
-            operator: '>',
-            duration: duration
-          }
-        };
-
+        var config = getConfig(4, '>', duration);
         expect(conditionDelegate(config)).toBe(true);
         assertCorrectMethodCall();
       });
 
       it('returns false when number of page views is below "greater than" constraint', function() {
-        var config = {
-          conditionConfig: {
-            count: 6,
-            operator: '>',
-            duration: duration
-          }
-        };
-
+        var config = getConfig(6, '>', duration);
         expect(conditionDelegate(config)).toBe(false);
         assertCorrectMethodCall();
       });
 
       it('returns true when number of page views is below "less than" constraint', function() {
-        var config = {
-          conditionConfig: {
-            count: 6,
-            operator: '<',
-            duration: duration
-          }
-        };
-
+        var config = getConfig(6, '<', duration);
         expect(conditionDelegate(config)).toBe(true);
         assertCorrectMethodCall();
       });
 
       it('returns false when number of page views is above "less than" constraint', function() {
-        var config = {
-          conditionConfig: {
-            count: 4,
-            operator: '<',
-            duration: duration
-          }
-        };
-
+        var config = getConfig(4, '<', duration);
         expect(conditionDelegate(config)).toBe(false);
         assertCorrectMethodCall();
       });
 
       it('returns true when number of page views matches "equals" constraint', function() {
-        var config = {
-          conditionConfig: {
-            count: 5,
-            operator: '=',
-            duration: duration
-          }
-        };
-
+        var config = getConfig(5, '=', duration);
         expect(conditionDelegate(config)).toBe(true);
         assertCorrectMethodCall();
       });
 
       it('returns false when number of page views does not match "equals" constraint', function() {
-        var config = {
-          conditionConfig: {
-            count: 11,
-            operator: '=',
-            duration: duration
-          }
-        };
-
+        var config = getConfig(11, '=', duration);
         expect(conditionDelegate(config)).toBe(false);
         assertCorrectMethodCall();
       });

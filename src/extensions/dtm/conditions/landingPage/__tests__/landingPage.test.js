1d2ae69f09c35b92bd19bef09823ea983686e9c8
Aaron Hardy <aahardy@adobe.com>
2015-09-03 11:27:20 -0600

    Breaking out creation of config object into shared function.

--- src/extensions/dtm/conditions/landingPage/__tests__/landingPage.test.js
+++ src/extensions/dtm/conditions/landingPage/__tests__/landingPage.test.js
@@ -13,44 +13,32 @@ var conditionDelegate = conditionDelegateInjector({
   'dtm/visitorTracking': mockVisitorTracking
 });
 
+function getConfig(pages) {
+  return {
+    conditionConfig: {
+      pages: pages
+    }
+  };
+}
+
 describe('landing page condition delegate', function() {
   it('returns true when the landing page matches one of the string options', function() {
-    var config = {
-      conditionConfig: {
-        pages: ['http://foo.com/bar.html', 'http://landingpage.com/test.html']
-      }
-    };
-
+    var config = getConfig(['http://foo.com/bar.html', 'http://landingpage.com/test.html']);
     expect(conditionDelegate(config)).toBe(true);
   });
 
   it('returns false when the landing page does not match one of the string options', function() {
-    var config = {
-      conditionConfig: {
-        pages: ['http://foo.com/bar.html', 'http://bar.com/foo.html']
-      }
-    };
-
+    var config = getConfig(['http://foo.com/bar.html', 'http://bar.com/foo.html']);
     expect(conditionDelegate(config)).toBe(false);
   });
 
   it('returns true when the landing page matches one of the regex options', function() {
-    var config = {
-      conditionConfig: {
-        pages: ['http://foo.com/bar.html', /landingpage\.com\/t.st/i]
-      }
-    };
-
+    var config = getConfig(['http://foo.com/bar.html', /landingpage\.com\/t.st/i]);
     expect(conditionDelegate(config)).toBe(true);
   });
 
   it('returns false when the landing page does not match one of the regex options', function() {
-    var config = {
-      conditionConfig: {
-        pages: ['http://foo.com/bar.html', /f.o/i]
-      }
-    };
-
+    var config = getConfig(['http://foo.com/bar.html', /f.o/i]);
     expect(conditionDelegate(config)).toBe(false);
   });
 });

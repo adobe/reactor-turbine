e39032bc58dec020840c632b585081e5dbcad352
Aaron Hardy <aahardy@adobe.com>
2015-09-08 10:21:11 -0600

    Added logged in condition support.

---
+++ src/extensions/dtm/conditions/loggedIn/loggedIn.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var getDataElement = require('getDataElement');
+
+/**
+ * Logged in condition. Determines if the user is logged in.
+ * @param {Object} config
+ * @param {Object} config.conditionConfig Condition config.
+ * @param {string} config.conditionConfig.dataElementName The name of the data element identifying
+ * whether the user is logged in.
+ * @returns {boolean}
+ */
+module.exports = function(config) {
+  return getDataElement(config.conditionConfig.dataElementName, true);
+};
+

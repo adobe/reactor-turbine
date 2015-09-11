aa2990ecb1a41c9e9120ebc262e6410782afd9ca
Aaron Hardy <aahardy@adobe.com>
2015-09-11 12:48:27 -0600

    Fixing enum reference.

--- src/engine/utils/dataElementSafe.js
+++ src/engine/utils/dataElementSafe.js
@@ -3,7 +3,7 @@ var getCookie = require('./cookie/getCookie');
 
 var COOKIE_PREFIX = '_sdsat_';
 
-var length = {
+var storeLength = {
   PAGEVIEW: 'pageview',
   SESSION: 'session',
   VISITOR: 'visitor'
@@ -15,18 +15,18 @@ module.exports = function(key, length) {
   if (arguments.length > 2) {
     // setter
     var value = arguments[2];
-    if (length === length.PAGEVIEW) {
+    if (length === storeLength.PAGEVIEW) {
       pageviewCache[key] = value;
-    } else if (length === length.SESSION) {
+    } else if (length === storeLength.SESSION) {
       setCookie(COOKIE_PREFIX + key, value);
-    } else if (length === length.VISITOR) {
+    } else if (length === storeLength.VISITOR) {
       setCookie(COOKIE_PREFIX + key, value, 365 * 2);
     }
   } else {
     // getter
-    if (length === length.PAGEVIEW) {
+    if (length === storeLength.PAGEVIEW) {
       return pageviewCache[key];
-    } else if (length === length.SESSION || length === length.VISITOR) {
+    } else if (length === storeLength.SESSION || length === storeLength.VISITOR) {
       return getCookie(COOKIE_PREFIX + key);
     }
   }

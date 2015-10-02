19e589f50fe1a396b1b4eb49deb86c065509ee96
Aaron Hardy <aahardy@adobe.com>
2015-10-02 13:17:16 -0600

    [DTM-6730] Fixed iterating over object properties to ignore properties on object prototypes.

--- src/__tests__/helpers/objectPrototypeProperties.js
+++ src/__tests__/helpers/objectPrototypeProperties.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // This emulates propertes that a client's website might add to the object prototype. This is to
-// test that we are properly excluding them with looping over object properties.
+// test that we are properly excluding them while looping over object properties.
 Object.prototype.inheritedProperty1 = null;
 Object.prototype.inheritedProperty2 = 'foo';
 Object.prototype.inheritedProperty3 = 123;

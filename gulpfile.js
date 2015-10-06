8b774331f2cc35151a417fec733d437eb931117b
Aaron Hardy <aahardy@adobe.com>
2015-10-06 16:36:39 -0600

    Separated turbine testrunner into its own repo.

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,12 @@
 
 var gulp = require('gulp');
 var rename = require('gulp-rename');
-var replace = require('gulp-replace');
 var uglify = require('gulp-uglify');
 var gzip = require('gulp-gzip');
-var path = require('path');
-var KarmaServer = require('karma').Server;
 var webpack = require('webpack-stream');
 var DefinePlugin = require('webpack').DefinePlugin;
-
-var $ = require('gulp-load-plugins')();
+var eslint = require('gulp-eslint');
+require('turbine-gulp-testrunner')(gulp);
 
 gulp.task('buildEngine', function() {
   return gulp.src('./src/bootstrap.js')
@@ -53,76 +50,19 @@ gulp.task('compressEngine', ['buildEngine'], function() {
     .pipe(gulp.dest('./dist'));
 });
 
-function startKarma(processor) {
-  var options = {
-    configFile: path.join(__dirname, 'karma.conf.js'),
-    webpack: {
-      devtool: '#inline-source-map',
-      externals: [
-        {
-          // So that modules can require('window') and then tests can mock it.
-          window: 'window',
-          document: 'document'
-        }
-      ]
-    }
-  };
-
-  if (processor) {
-    processor(options);
-  }
-
-  new KarmaServer(options).start();
-}
-
-// Typically we wouldn't be dependent upon building the full engine and config (by running the
-// "default" task first) since Karma would dynamically compile the pieces under test, but because
-// we have functional tests that work within iframes and are dependent upon the full engine and
-// config, this is necessary.
-gulp.task('test', ['default'], function() {
-  startKarma();
-});
-
-gulp.task('coverage', ['default'], function() {
-  startKarma(function(options) {
-    options.webpack.module = {
-      preLoaders: [{
-        test: /\.js$/,
-        exclude: /(__tests__)\//,
-        loader: 'istanbul-instrumenter'
-      }]
-    };
-    options.reporters = ['progress', 'coverage'];
-    options.singleRun = true;
-  });
-});
-
-gulp.task('testall', ['default'], function() {
-  startKarma(function(options) {
-    options.browsers = [
-      'Chrome',
-      'Firefox',
-      'Safari',
-      'IE9 - Win7',
-      'IE10 - Win7',
-      'IE11 - Win7'
-    ];
-  });
-});
-
 gulp.task('watch', function() {
   gulp.watch(['./src/{,**/!(__tests__)}/*.js'], ['buildEngine']);
 });
 
 gulp.task('lint', function() {
   return gulp.src('./src/**/*.js')
-    .pipe($.eslint())
+    .pipe(eslint())
     // eslint.format() outputs the lint results to the console.
     // Alternatively use eslint.formatEach() (see Docs).
-    .pipe($.eslint.format('stylish'))
+    .pipe(eslint.format('stylish'))
     // To have the process exit with an error code (1) on
     // lint error, return the stream and pipe to failOnError last.
-    .pipe($.eslint.failAfterError());
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('default', ['buildEngine', 'watch']);

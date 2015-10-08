777a1c2b2da5a242b7da60f4513ccd3f1584c659
Aaron Hardy <aahardy@adobe.com>
2015-10-08 15:05:01 -0600

    Making turbine compatible with npm 2 when it's a dependency of another project.

--- tasks/buildEngine.js
+++ tasks/buildEngine.js
@@ -32,7 +32,14 @@ module.exports = function(gulp) {
           new DefinePlugin({
             ENV_TEST: true
           })
-        ]
+        ],
+        // npm 2 compatibility. If this weren't here, webpack would look for loaders in the
+        // node_modules directly under the project requiring turbine. We want to look under
+        // the turbine's node_modules. In npm 3, they're the same directory since
+        // turbine's node_modules is a symlink to the project's node_modules.
+        resolveLoader: {
+          root: path.join(__dirname, '../node_modules')
+        }
       }))
       .pipe(gulp.dest('dist'));
   });

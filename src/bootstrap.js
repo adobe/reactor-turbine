faeb26cb2ce5b8b4de86ca6a93f11d1e16a98358
Aaron Hardy <aahardy@adobe.com>
2015-10-09 21:37:49 -0600

    Removing old useless comment.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -12,8 +12,4 @@ state.init(window._satellite.container); // Must come first.
 require('./hydrateSatelliteObject')();
 require('./utils/preprocessConfig').init(state.getPropertyConfig().undefinedVarsReturnEmpty);
 require('./utils/logger').outputEnabled = state.getDebugOutputEnabled();
-
-// TODO: For use during development in order to see errors in the console from rule execution.
-// require('./utils/logger').outputEnabled = true;
-
 require('./initRules')(); // Must come last.

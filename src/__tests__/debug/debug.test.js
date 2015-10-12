42bcef706bee1f8b2643e183fc6eb08ee3c42d5f
Aaron Hardy <aahardy@adobe.com>
2015-10-12 14:22:32 -0600

    Fixed test names.

--- src/__tests__/debug/debug.test.js
+++ src/__tests__/debug/debug.test.js
@@ -1,8 +1,8 @@
 'use strict';
 
 describe('sdsat_debug local storage setting', function() {
-  runTestPage('logs to the console when set to true', __dirname + '/sdsatDebugFalse.html');
-  runTestPage('does not log to console when not set', __dirname + '/sdsatDebugTrue.html');
+  runTestPage('does not log to console when not set', __dirname + '/sdsatDebugFalse.html');
+  runTestPage('logs to the console when set to true', __dirname + '/sdsatDebugTrue.html');
 });
 
 describe('setDebug', function() {

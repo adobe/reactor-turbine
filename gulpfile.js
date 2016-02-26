58052262fb2d6cb9dc43a0b454aefbd0189b8c3a
Aaron Hardy <aahardy@adobe.com>
2016-02-26 14:20:37 -0700

    Removing sandbox as a dependency since nobody runs it from Turbine anyway.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 
 var gulp = require('gulp');
 require('@reactor/turbine-gulp-testrunner')(gulp);
-require('@reactor/turbine-gulp-sandbox')(gulp);
 gulp.task('build', require('./tasks/createBuildTask')());
 gulp.task('compress', ['build'], require('./tasks/compress'));
 gulp.task('default', ['build']);

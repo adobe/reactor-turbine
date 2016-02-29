6c1cad9c545d32fd16ada8ae04823da7f25dfa1c
Serban Stancu <stancu@adobe.com>
2016-02-29 16:27:04 -0700

    Add tests for resourceProvider.

---
+++ src/__tests__/resourceProvider.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var resourceProviderInjector = require('inject!../resourceProvider');
+var resourceProvider;
+var moduleExport = function() {};
+
+describe('resource provider', function() {
+  beforeAll(function() {
+    resourceProvider = resourceProviderInjector({
+      './extractModuleExports': jasmine.createSpy('extractModuleExports').and.callFake(function() {
+        return moduleExport;
+      })
+    });
+  });
+
+  it('should return the resource using the provided name', function() {
+    var resource = jasmine.createSpy('resource');
+    resourceProvider.addResource('dtm/resources/somename', resource);
+
+    expect(resourceProvider.getResource('dtm', 'somename')).toBe(resource);
+  });
+
+  it('should add module exports to resource', function() {
+    var delegate = jasmine.createSpy('delegate');
+    resourceProvider.addResource('dtm/resources/somename', delegate);
+
+    expect(resourceProvider.getResource('dtm', 'somename').exports).toBe(moduleExport);
+  });
+});

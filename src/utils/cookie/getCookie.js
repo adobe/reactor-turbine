c7a8323dce0d0b258f33b97c1f3dc981ca160e40
Aaron Hardy <aahardy@adobe.com>
2016-03-02 13:03:29 -0700

    Replaced cookie code with cookie npm package.

--- src/utils/cookie/getCookie.js
+++ src/utils/cookie/getCookie.js
@@ -1,19 +1,10 @@
+var cookie = require('cookie');
+
 /**
  * Reads a cookie value.
  * @param {string} name The name of the cookie to read.
  * @returns {string}
  */
 module.exports = function(name) {
-  var nameEQ = name + '=';
-  var parts = document.cookie.split(';');
-  for (var i = 0; i < parts.length; i++) {
-    var c = parts[i];
-    while (c.charAt(0) === ' ') {
-      c = c.substring(1, c.length);
-    }
-    if (c.indexOf(nameEQ) === 0) {
-      return c.substring(nameEQ.length, c.length);
-    }
-  }
-  return null;
+  return cookie.parse(document.cookie)[name];
 };

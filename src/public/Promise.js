9d1260b41d0639e38ca907f2b3fd0495f046adcf
Aaron Hardy <aahardy@adobe.com>
2016-06-21 16:47:13 -0600

    Swapping out WeakMapap implementation with the one from polymer and preventing both WeakMap and Promise from polluting global scope.

--- src/public/Promise.js
+++ src/public/Promise.js
@@ -1,4 +1,11 @@
 // Polyfill for Promises, staying as close as possible to what's specified in both Promises/A+ and
 // the upcoming ES6 specification.
+// native-promise-only will set window.Promise if it doesn't exist. In our case, we don't want to
+// be setting globals so we'll return the global back to prior value after native-promise-only
+// is run.
 
-module.exports = require('native-promise-only');
+var priorGlobal = window.Promise;
+var Promise = require('native-promise-only');
+window.Promise = priorGlobal;
+
+module.exports = Promise;

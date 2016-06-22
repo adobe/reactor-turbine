3f4d05ab81b96f4526d0b955690509b8f6d3fb45
Aaron Hardy <aahardy@adobe.com>
2016-06-22 13:59:46 -0600

    Adding logic to only use native-promise-only if native implementation doesn't exist.

--- src/public/Promise.js
+++ src/public/Promise.js
@@ -1,11 +1,14 @@
 // Polyfill for Promises, staying as close as possible to what's specified in both Promises/A+ and
 // the upcoming ES6 specification.
-// native-promise-only will set window.Promise if it doesn't exist. In our case, we don't want to
-// be setting globals so we'll return the global back to prior value after native-promise-only
-// is run.
 
-var priorGlobal = window.Promise;
-var Promise = require('native-promise-only');
-window.Promise = priorGlobal;
+var window = require('window');
+var Promise = window.Promise;
+
+if (typeof Promise === 'undefined') {
+  Promise = require('native-promise-only');
+  // native-promise-only will set window.Promise if it doesn't exist. In our case,
+  // we don't want to be setting globals so we'll return the global back to undefined.
+  window.Promise = undefined;
+}
 
 module.exports = Promise;

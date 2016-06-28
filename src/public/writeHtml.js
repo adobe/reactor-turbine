0353b857a7d4f988bd02b1242deed1430c350455
Aaron Hardy <aahardy@adobe.com>
2016-06-28 11:58:50 -0600

    Changed write-html core module to throw errors when unable to write to the document.

--- src/public/writeHtml.js
+++ src/public/writeHtml.js
@@ -1,20 +1,18 @@
 /*eslint no-alert:0*/
 'use strict';
-var logger = require('./logger');
 var hasDomContentLoaded = require('./hasDomContentLoaded');
+var document = require('document');
 
 module.exports = function(html) {
   // Document object in XML files is different from the ones in HTML files. Documents served with
   // the `application/xhtml+xml` MIME type don't have the `document.write` method.
   // More info: https://www.w3.org/MarkUp/2004/xhtml-faq#docwrite or https://developer.mozilla.org/en-US/docs/Archive/Web/Writing_JavaScript_for_HTML
   if (!document.write) {
-    logger.error('Cannot write HTML to the page. `document.write` is not available.');
-    return;
+    throw new Error('Cannot write HTML to the page. `document.write` is unavailable.');
   }
 
   if (hasDomContentLoaded()) {
-    logger.error('Cannot call `document.write` after `DOMContentloaded` was fired.');
-    return;
+    throw new Error('Cannot call `document.write` after `DOMContentloaded` has fired.');
   }
 
   document.write(html);

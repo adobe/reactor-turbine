df59a50339225f8b95d3d4862c81a5c41aae143f
Aaron Hardy <aahardy@adobe.com>
2016-06-29 19:17:43 -0600

    Fixed hasDomContentLoaded for IE.

--- src/public/__tests__/hasDomContentLoaded.test.js
+++ src/public/__tests__/hasDomContentLoaded.test.js
@@ -8,10 +8,25 @@ describe('hasDomContentLoaded', function() {
     var injectHasDomContentLoaded = require('inject!../hasDomContentLoaded');
     var hasDomContentLoaded = injectHasDomContentLoaded({
       'document': {
-        readyState: 'complete'
+        addEventListener: function(type, callback) {
+          if (type === 'DOMContentLoaded') {
+            callback();
+          }
+        }
       }
     });
 
     expect(hasDomContentLoaded()).toBe(true);
+  });
+
+  it('returns false before DOM content was loaded', function() {
+    var injectHasDomContentLoaded = require('inject!../hasDomContentLoaded');
+    var hasDomContentLoaded = injectHasDomContentLoaded({
+      'document': {
+        addEventListener: function() {}
+      }
+    });
+
+    expect(hasDomContentLoaded()).toBe(false);
   });
 });

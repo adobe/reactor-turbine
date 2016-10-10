c43997826d5ea82478ca7678a18bdfe6d4b1c968
Serban Stancu <stancu@adobe.com>
2016-10-10 12:57:49 -0600

    Use native Object.assign when available.

--- src/public/assign.js
+++ src/public/assign.js
@@ -1,14 +1,21 @@
-module.exports = function(target) {
-  for (var i = 1; i < arguments.length; i++) {
-    var from = arguments[i];
-    if (from === null || from === undefined) {
-      continue;
+var window = require('window');
+var assign = window.Object.assign;
+
+if (typeof assign === 'undefined') {
+  assign = function(target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var from = arguments[i];
+      if (from === null || from === undefined) {
+        continue;
+      }
+      var keys = Object.keys(from);
+      for (var j = 0; j < keys.length; j++) {
+        var key = keys[j];
+        target[key] = from[key];
+      }
     }
-    var keys = Object.keys(from);
-    for (var j = 0; j < keys.length; j++) {
-      var key = keys[j];
-      target[key] = from[key];
-    }
-  }
-  return target;
-};
+    return target;
+  };
+}
+
+module.exports = assign;

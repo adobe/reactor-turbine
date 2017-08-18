e694cad0a5256c53d59b2532bf9a7b8299299780
Aaron Hardy <aahardy@adobe.com>
2017-08-18 10:50:30 -0600

    Removing container from _satellite after initialization.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -12,19 +12,12 @@
 
 var state = require('./state');
 
-if (!window._satellite) {
-  window._satellite = {
-    container: {
-      rules: []
-    }
-  };
-}
-
 require('./hydrateSatelliteObject')(
   window._satellite.container.buildInfo,
   state.setDebugOutputEnabled
 );
 
 state.init(window._satellite.container); // Must come first.
+delete window._satellite.container;
 require('./logger').outputEnabled = state.getDebugOutputEnabled();
 require('./initRules')(); // Must come last.

36ee0610b9d466f0db43f248f56d53d957beb258
Aaron Hardy <aaron@aaronhardy.com>
2017-09-01 16:55:17 -0600

    Limiting js-cookie API exposure. (#22)

--- src/public/cookie.js
+++ src/public/cookie.js
@@ -10,4 +10,14 @@
  * governing permissions and limitations under the License.
  ****************************************************************************************/
 
-module.exports = require('js-cookie');
+var cookie = require('js-cookie');
+
+// js-cookie has other methods that we haven't exposed here. By limiting the exposed API,
+// we have a little more flexibility to change the underlying implementation later. If clear
+// use cases come up for needing the other methods js-cookie exposes, we can re-evaluate whether
+// we want to expose them here.
+module.exports = {
+  get: cookie.get,
+  set: cookie.set,
+  remove: cookie.remove
+};

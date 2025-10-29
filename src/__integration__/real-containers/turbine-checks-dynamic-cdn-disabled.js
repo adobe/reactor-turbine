f1a473ae4eaa52d3d64e263b042f655835c22b3d
Brent Hosie <bhosie@adobe.com>
2025-10-29 17:05:27 -0600

    pdcl-13739: clarify the turbine tests. renamed containers. test customCodeTransforms when premium cdn disabled.

---
+++ src/__integration__/real-containers/turbine-checks-dynamic-cdn-disabled.js
@@ -0,0 +1,89 @@
+/***************************************************************************************
+ * (c) 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ ****************************************************************************************/
+
+const libraryBuildPathsFile = require('../../../ci-scripts/library-build-paths.json');
+const loadHtml = require('../load-page-environment-html');
+const { test, expect } = require('@playwright/test');
+const setupTurbineEventListener = require('./setup-turbine-event-listener');
+
+test('Verify turbine-free-vars rules', async ({ page }) => {
+  await loadHtml({ page });
+  const expectedActionIds = [
+    'TurbineFreeVars::turbine_cc_data_element-pass::thrownError',
+    'TurbineEventCustomCodeRule::sequence-event-js-1-pass::thrownError',
+    'TurbineEventCustomCodeRule::sequence-event-js-2-pass',
+    'TurbineConditionCustomCodeRule::sequence-condition-js-1-pass::thrownError',
+    'TurbineConditionCustomCodeRule::sequence-action-js-1-pass',
+    'TurbineEmbeddedActionCustomCodeRule::sequence-action-js-1-pass::thrownError',
+    'TurbineLinkedActionCustomCode::sequence-action-js-1-pass::thrownError'
+  ];
+  const resultsPromise = setupTurbineEventListener({
+    page,
+    expectedActionIds
+  });
+
+  // Inject the script dynamically (don't navigate away)
+  await page.addScriptTag({
+    url: libraryBuildPathsFile.TURBINE_CHECKS_CDN_DISABLED.libraryLink
+  });
+
+  const { expectedActionsFound, unexpectedActionsFound } = await resultsPromise;
+  expect(expectedActionsFound.length).toBe(expectedActionIds.length);
+  expectedActionsFound.forEach(({ actionId: a }) => {
+    expect(expectedActionIds.includes(a)).toBe(true);
+  });
+  expect(unexpectedActionsFound.length).toBe(0);
+});
+
+test('custom code transforms on standard host (not dynamicCDN)', async ({
+  page
+}) => {
+  await loadHtml({ page });
+
+  await page.addScriptTag({
+    url: libraryBuildPathsFile.TURBINE_CHECKS_CDN_DISABLED.libraryLink
+  });
+
+  const rules = await page.evaluate(() => window._satellite._container.rules);
+
+  let isLibraryLoadedCustomCodeAssertions = 0;
+  let allOtherEventCustomCodeAssertions = 0;
+
+  rules.forEach((rule) => {
+    // for simplicity in our library
+    expect(rule.events.length).toBe(1);
+    expect(rule.actions.length).toBe(1);
+
+    const [event] = rule.events;
+    const [action] = rule.actions;
+
+    const isLibraryLoadedOrPageBottomEvent =
+      event.modulePath.includes('pageBottom') ||
+      event.modulePath.includes('libraryLoaded');
+    const isCustomCodeAction = action.modulePath.includes('customCode');
+    if (isLibraryLoadedOrPageBottomEvent) {
+      if (isCustomCodeAction) {
+        expect(action.settings.source.includes('https://')).toBe(false);
+        isLibraryLoadedCustomCodeAssertions++;
+      }
+    } else {
+      // every other event type
+      if (isCustomCodeAction) {
+        expect(action.settings.source.includes('https://')).toBe(true);
+        allOtherEventCustomCodeAssertions++;
+      }
+    }
+  });
+
+  expect(isLibraryLoadedCustomCodeAssertions).toBeGreaterThan(0);
+  expect(allOtherEventCustomCodeAssertions).toBeGreaterThan(0);
+});
